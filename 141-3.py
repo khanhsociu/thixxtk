868e9afc655532d1361c5479d5ed0a4140aea20f
khanhsociu <84558770+khanhsociu@users.noreply.github.com>
2022-01-14 11:06:47 +0700

    Add files via upload

---
+++ 141-3.py
@@ -0,0 +1,9 @@
+def str_split(s):
+    list_result = []
+    for i in s:
+        list_result.append(i)
+    tuple_result = tuple(list_result)
+    return list_result, tuple_result
+
+
+print(str_split('lucphikhanh1451020121'))
